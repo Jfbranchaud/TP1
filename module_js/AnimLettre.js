420d1ce118f69a27c513a0dcd4d952e87bb654e1
jeffxsi <jfbranchaud40@gmail.com>
2019-02-12 12:28:15 -0500

    animations lettres

---
+++ module_js/AnimLettre.js
@@ -0,0 +1,77 @@
+import {
+    Util
+} from './Util.js'
+
+
+export class AnimLettre {
+
+    /**
+     * Classe permettant de créer et d'animer une introduction
+     * @param {string} lesLettres - contient l'ensemble des mots d'intro
+     * @param {DOMElement} elementParent - Conteneur de l'animation
+     * @param {function} fonction - l'adresse de la fonction à exécuter après l'animation
+         
+     }}
+     */
+
+    constructor(lesLettres, elementParent, fonction) {
+        //Récupérer les valeurs passées en paramètre			
+        
+        this.lesLettres = lesLettres;
+        this.elmParent = elementParent;
+        this.anim_lettres(this.lesLettres);
+        this.fonction = fonction;
+    }
+
+
+    anim_lettres(lesLettres) {
+        /* Création des élément DOM qui seront animés. 
+        Les éléments seront intégré dans le conteneur elmParent
+        */  
+       let i = 0;
+        console.log('mot')
+        let elmConteneur = this.creerElement(this.elmParent,
+            'section',
+            '',
+            'mot')
+
+            for (let lettre of lesLettres){
+			let elmLettre = this.creerElement(elmConteneur,'div', lettre, '')
+            elmLettre.style.animationDelay = (i++ * 0.5) + "s";
+			//elmLettre.style.color = tabCouleur[(i++)%7]
+                
+            }
+          
+
+
+	
+	
+    }
+
+    creerElement(elmParent, balise, contenu, classCSS) {
+        console.log(balise)
+        let noeud = document.createElement(balise)
+        if (contenu != '') {
+            noeud.innerHTML = contenu
+        }
+        if (classCSS != '') {
+            noeud.classList.add(classCSS)
+        }
+
+    
+      
+        elmParent.appendChild(noeud)
+        return noeud
+    }
+
+    terminerIntro(evt) {
+        this.elmParent.firstChild.classList.add('deplacementContenuIntro')
+        this.elmParent.firstChild.addEventListener('animationend', this.passerVersAnimationSuivante.bind(this))
+    }
+
+    passerVersAnimationSuivante(evt) {
+        Util.detruireTousLesNoeud(this.elmParent, this.elmParent)
+        this.fonction()
+    }
+
+}
